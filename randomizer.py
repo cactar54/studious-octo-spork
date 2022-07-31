26eb3cfddca6c2c1b8a59150ece54337ad4d5eaf
cactar54 <cactar54@aol.com>
2022-07-31 15:58:02 -0400

    Added functionality to randomizeOrder in randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -1,7 +1,16 @@
+import random
+
 def randomizeOrder(array):
     randomizedOrder = []
+    arrayLength = len(array)
+    usedNumbers = []
+    randomNumber = 0
 
-
+    while len(usedNumbers) < arrayLength:
+        randomNumber = random.randrange(0, arrayLength, 1)
+        if randomNumber not in usedNumbers:
+            randomizedOrder.append(array[randomNumber])
+            usedNumbers.append(randomNumber)
 
     return randomizedOrder
 
