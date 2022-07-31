33e6efa1462b660d1028198d864e4c3704b33573
cactar54 <cactar54@aol.com>
2022-07-31 15:41:36 -0400

    Added randomizeSpelling and randomizeOrder functions to randomizer.py Imported randomizeSpelling and randomizeOrder functions in main.py Added randomizeSpelling and randomizeOrder calls and prints to main.py

---
+++ randomizer.py
@@ -0,0 +1,18 @@
+def randomizeOrder(array):
+    randomizedOrder = []
+
+
+
+    return randomizedOrder
+
+
+
+
+
+
+
+def randomizeSpelling(array):
+    randomizedSpelling = []
+
+
+    return randomizedSpelling
