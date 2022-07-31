a76574943648c6357e5e4721906faecfe5a8c810
cactar54 <cactar54@aol.com>
2022-07-31 13:54:34 -0400

    Added sort to main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 if __name__ == "__main__":
     animals = ["Horse", "Dog", "Cat", "Panda", "Snake"]
+
+    #Not 100% sure if this is what you mean when you said "You will write code that will manipulate an array, you will commit your changes in source tree, and push them to github"
+    animals.sort()
