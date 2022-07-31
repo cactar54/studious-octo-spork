8d279919b3aae6ab10b9f0f4659c318303dba8c2
cactar54 <cactar54@aol.com>
2022-07-31 14:01:43 -0400

    Added reverse print to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from reverse import reverse
+
 if __name__ == "__main__":
     animals = ["Horse", "Dog", "Cat", "Panda", "Snake"]
 
-    #Not 100% sure if this is what you mean when you said "You will write code that will manipulate an array, you will commit your changes in source tree, and push them to github"
-    animals.sort()
+    print(reverse(animals))
