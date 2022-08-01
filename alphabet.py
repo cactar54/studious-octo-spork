643b8d5c57c03dc004576c72001144b6da94b6b8
cactar54 <cactar54@aol.com>
2022-07-31 20:47:14 -0400

    Simplified alphabet.py code

--- alphabet.py
+++ alphabet.py
@@ -1,18 +1,8 @@
 def alphabet(array, letter):
     matchingItems = []
-    firstLetters = []
-    position = 0
 
     for item in array:
-        firstLetters.append(array[position][0])
-        if letter.isupper():
-            if firstLetters[position] == letter:
-                matchingItems.append(item)
-
-        if letter.islower():
-            if firstLetters[position] == letter.upper():
-                matchingItems.append(item)
-
-        position += 1
+        if item[0].lower() == letter.lower():
+            matchingItems.append(item)
 
     return matchingItems
